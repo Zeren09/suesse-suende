73bf5e15e9288dbd13a80149d4f3668f603471af
Zeren09 <zerenkurban2006@gmail.com>
2025-05-21 18:24:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        } 
+    }
+  });
